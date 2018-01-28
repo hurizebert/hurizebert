125ac6c6f9a93d296a6c26f2f190fe41fb2137eb
Gilbert Emerson <emer7@users.noreply.github.com>
2018-01-28 09:49:06 +0800

    add 3 new lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
+  get 'home/ewl'
+  get 'home/nsl'
+  get 'home/ccl'
+  get 'home/bplrt'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
