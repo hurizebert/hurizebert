ade3d1e7a8fbc5ddac717ab89f98d9a7482c09e1
Gilbert Emerson <emer7@users.noreply.github.com>
2018-01-28 08:11:15 +0800

    JSON parsing and present to view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    jsonFiles = File.read("EWL.json")
+    jsonArray = JSON.parse(jsonFiles)
+    timestampArray = []
+    jsonArray.each do |jsonItem|
+      timestampObj = DateTime.strptime(jsonItem['timestamp'], '%Y-%m-%dT%H:%M:%S')
+      timestampObj = timestampObj + 8.hours
+      timestampArray.push((timestampObj.change(:offset => '+0800')).to_date)
+      timestampArray = timestampArray.uniq.sort()
+    end
+    @timestampArray = timestampArray
   end
 end
